b9c6b3768c1fea9f5b8c4ce1466c7ae9494f0d59
Vikrant Shete <tejuviky2005@gmail.com>
2025-08-03 14:53:34 +0530

    correct placement of early return handlers. Added resize handlers to table

--- src/app/comonents/QueryResult.tsx
+++ src/app/comonents/QueryResult.tsx
@@ -27,6 +27,49 @@ export const QueryResult = ({
     errorMessage,
     isDataLoading,
 }: QueryReultProps) => {
+
+    const columns: ColumnDef<string[]>[] = useMemo(() => data?.[0].map((col, colIndex) => ({
+        accessorKey: colIndex.toString(),
+        header: col,
+        cell: (info) => info.getValue(),
+        sortUndefined: 'last'
+    })), [data]) || [];
+
+    const tableData = useMemo(() => data?.slice(1), [data]) || [];
+
+    const [sorting, setSorting] = React.useState<SortingState>([]);
+
+    const table = useReactTable({
+        data: tableData,
+        columns,
+        state: {
+            sorting,
+        },
+        onSortingChange: setSorting,
+        getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        getPaginationRowModel: getPaginationRowModel(), // Enable pagination
+        enableColumnResizing: true,
+        columnResizeMode: "onChange",
+    });
+
+    /**
+   * Instead of calling `column.getSize()` on every render for every header
+   * and especially every data cell (very expensive),
+   * we will calculate all column sizes at once at the root table level in a useMemo
+   * and pass the column sizes down as CSS variables to the <table> element.
+   */
+    const columnSizeVars = React.useMemo(() => {
+        const headers = table.getFlatHeaders()
+        const colSizes: { [key: string]: number } = {}
+        for (let i = 0; i < headers.length; i++) {
+            const header = headers[i]!
+            colSizes[`--header-${header.id}-size`] = header.getSize()
+            colSizes[`--col-${header.column.id}-size`] = header.column.getSize()
+        }
+        return colSizes
+    }, [table.getState().columnSizingInfo, table.getState().columnSizing])
+
     if (isDataLoading) {
         return (
             <div className="query-result flex justify-center items-center mt-10">
@@ -56,35 +99,10 @@ export const QueryResult = ({
         return <div className="query-result text-red-500">{errorMessage}</div>;
     }
 
-    const columns: ColumnDef<string[]>[] = useMemo(() => data[0].map((col, colIndex) => ({
-        accessorKey: colIndex.toString(),
-        header: col,
-        cell: (info) => info.getValue(),
-        sortUndefined: 'last'
-    })), [data]);
-
-    const tableData = useMemo(() => data.slice(1), [data]);
-
-    const [sorting, setSorting] = React.useState<SortingState>([]);
-
-    const table = useReactTable({
-        data: tableData,
-        columns,
-        state: {
-            sorting,
-        },
-        onSortingChange: setSorting,
-        getCoreRowModel: getCoreRowModel(),
-        getSortedRowModel: getSortedRowModel(),
-        getPaginationRowModel: getPaginationRowModel(), // Enable pagination
-        enableColumnResizing: true,
-        columnResizeMode: "onChange",
-    });
-
     return (
         <div>
             <div className="query-result w-full overflow-x-auto scroll-smooth">
-                <table className="table-auto border-collapse border border-gray-300">
+                <table className="table-auto border-collapse border border-gray-300" style={{ ...columnSizeVars }}>
                     <thead>
                         {table.getHeaderGroups().map((headerGroup) => (
                             <tr key={headerGroup.id} className="bg-gray-200">
@@ -93,14 +111,14 @@ export const QueryResult = ({
                                         key={header.id}
                                         className="border border-gray-300 py-5 px-3 text-left whitespace-nowrap min-w-[150px] cursor-pointer"
                                         style={{
-                                            width: header.getSize(),
+                                            width: `calc(var(--header-${header?.id}-size) * 1px)`,
                                         }}
                                     >
                                         <div
-                                            className={
-                                                header.column.getCanSort()
+                                            className={`
+                                                ${header.column.getCanSort()
                                                     ? 'cursor-pointer select-none'
-                                                    : ''
+                                                    : ''} group relative `
                                             }
                                             onClick={header.column.getToggleSortingHandler()}
                                             title={
@@ -119,6 +137,14 @@ export const QueryResult = ({
                                                     header.column.columnDef.header,
                                                     header.getContext()
                                                 )}
+                                            <div
+                                                {...{
+                                                    onDoubleClick: () => header.column.resetSize(),
+                                                    onMouseDown: header.getResizeHandler(),
+                                                    onTouchStart: header.getResizeHandler(),
+                                                    className: `resizer absolute top-0 h-full right-0 w-[5px] cursor-col-resize select-none touch-none opacity-0 group-hover:opacity-100 transition-opacity duration-200 ${header.column.getIsResizing() ? 'bg-blue-500 opacity-100' : 'bg-black bg-opacity-50'}`,
+                                                }}
+                                            />
                                         </div>
                                     </th>
                                 ))}
@@ -135,6 +161,9 @@ export const QueryResult = ({
                                     <td
                                         key={cell.id}
                                         className="border border-gray-300 whitespace-nowrap min-w-[150px] p-3"
+                                        style={{
+                                            width: `calc(var(--col-${cell.column.id}-size) * 1px)`,
+                                        }}
                                     >
                                         {flexRender(
                                             cell.column.columnDef.cell,
