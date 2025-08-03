1b1e354fc88410d5582da01a22f3512c49999e43
Vikrant Shete <tejuviky2005@gmail.com>
2025-08-03 17:16:32 +0530

    Hook to download the file

---
+++ src/app/hooks/useFileDownload.ts
@@ -0,0 +1,25 @@
+/**
+ * A custom hook to handle file download functionality.
+ * @param data - The 2D string array data to be downloaded as CSV
+ * @returns - A function to handle file download
+ */
+export const useFileDownload = (data?: string[][]) => {
+    const onDownload = () => {
+        const csvData = data?.map(row => row.join(",")).join("\n");
+        // Download the data as csv
+        if (!csvData) {
+            alert("No data to download.");
+            return;
+        }
+        
+        const blob = new Blob([csvData], { type: "text/csv;charset=utf-8;" });
+        
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.setAttribute("download", "query_results.csv");
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+    return { onDownload };
+}
